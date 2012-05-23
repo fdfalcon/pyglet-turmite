a4af00ad0c721a82adf549403b3eaf5cde208824
Francisco Falcon <fdfalcon@gmail.com>
2012-05-22 21:22:29 -0300

    Adding license.

--- pyglet-turmite.py
+++ pyglet-turmite.py
@@ -1,9 +1,39 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+#------------------------------------------------------------------------
+#Copyright (c) 2012, Francisco Falcon
+#All rights reserved.
+#
+#Redistribution and use in source and binary forms, with or without
+#modification, are permitted provided that the following conditions are met:
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of the copyright holder nor the
+#      names of its contributors may be used to endorse or promote products
+#      derived from this software without specific prior written permission.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+#ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+#DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#------------------------------------------------------------------------
+
+
 
 # Rules (http://en.wikipedia.org/wiki/Langton%27s_ant)
 #
-# At a white square, turn 90บ right, flip the color of the square, move forward one unit
-# At a black square, turn 90บ left, flip the color of the square, move forward one unit
+# At a white square, turn 90ยบ right, flip the color of the square, move forward one unit
+# At a black square, turn 90ยบ left, flip the color of the square, move forward one unit
 #
 
 from pyglet import window, clock, font, gl, graphics
@@ -116,11 +146,11 @@ class Grid(window.Window):
 
 
 	def move(self):
-		#Is the ant on a white cell? => Make the cell black, turn 90บ right, move forward one cell
+		#Is the ant on a white cell? => Make the cell black, turn 90ยบ right, move forward one cell
 		if self.cells[self.ant.posy][self.ant.posx]:
 			self.cells[self.ant.posy][self.ant.posx] = False
 			self.ant.turn('right')
-		#Is the ant on a black cell? => Make the cell white, turn 90บ left, move forward one cell
+		#Is the ant on a black cell? => Make the cell white, turn 90ยบ left, move forward one cell
 		else:
 			self.cells[self.ant.posy][self.ant.posx] = True
 			self.ant.turn('left')
